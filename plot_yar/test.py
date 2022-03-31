bb53d4d729f6248782c90d7b5c6ae383527ed9b8
yarvod <yar.vod.ole@gmail.com>
2022-03-31 20:19:55 +0300

    upd

---
+++ plot_yar/test.py
@@ -0,0 +1,4 @@
+from plot import plot_approx
+
+
+plot_approx(X_data=[[1,2,4,5]], Y_data=[[2,3,5,9]], input_function='linear', approx_slice=[1.2,4.6])
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setup(name='plot_yar',
-      version='0.3',
+      version='0.4',
       description='Plotting_graphs',
       packages=['plot_yar'],
       author_email='vodyar00@mail.ru',
@@ -18,7 +18,7 @@ setup(name='plot_yar',
 	url="https://github.com/yarvod/plot_yar",
 	# Находит все пакеты внутри проекта и объединяет их в дистрибутив.
 	classifiers=[
-		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	])
